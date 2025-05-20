52ba466d11efb96f4c2123917d80afac5f68b6b5
BahAwa <bah.awaa@gmail.com>
2025-05-20 21:30:59 +0200

    refactor: clean up formatting in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,28 +2,22 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "cdn.jsdelivr.net",
-                port: "",
-                search: "",
-            },
-            {
-                protocol: "https",
-                hostname: "s3-starfire.s3.eu-west-3.amazonaws.com",
-                port: "",
-                pathname: "/**",
-            },
-        ],
-    },
-    // Add experimental configuration to increase the server actions body size limit
-    experimental: {
-        serverActions: {
-            bodySizeLimit: "4mb", // Increase to 4MB which should be enough for most image uploads
-        },
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+        port: "",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "s3-starfire.s3.eu-west-3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
